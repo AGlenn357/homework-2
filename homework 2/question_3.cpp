1645148fb67557fc8e6e7d8634e22ff6b750a239
AGlenn357 <andrewmaysglenn@gmail.com>
2023-11-08 15:02:06 -0500

    added libraries

--- homework 2/question_3.cpp
+++ homework 2/question_3.cpp
@@ -4,3 +4,7 @@ AERSP 424
 Homework 2
 Question 3: implementing plane class functions
 */
+
+#include <iostream> //including input output library
+#include <map> //including map library
+#include <string> //including string library
