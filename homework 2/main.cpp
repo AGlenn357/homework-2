2cae8664e979c337a08fb6abd28517b9f6ee3f01
AGlenn357 <andrewmaysglenn@gmail.com>
2023-11-08 13:17:09 -0500

    Initial Commit

---
+++ homework 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework 2
+//
+//  Created by Andrew Glenn on 11/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
